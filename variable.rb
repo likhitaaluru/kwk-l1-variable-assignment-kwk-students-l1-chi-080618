06004244abe953966c17527c43c4bccd7a940a24
likhitaaluru <likhitaaluru@gmail.com>
2018-08-06 21:05:12 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,4 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-local_variables = greeting
 greeting = "Hello World"
-puts "Hello World"
+puts "#{greeting}""
